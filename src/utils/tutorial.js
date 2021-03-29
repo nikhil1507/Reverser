529d0bc2dc1b8049b9768874eafae12067465929
Nikhil Thakur <nikhilthakur@samplemail.com>
2021-03-29 09:42:01 +0530

    Added the assets

---
+++ src/utils/tutorial.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 0,
+    tex: `If you want to dive right in, feel free to press the "Skip Tutorial"
+    button below. Otherwise, press "Next"!`,
+  },
+  {
+    id: 1,
+    tex:
+      "The snake will grow once if it will eat it's food which is of green color",
+  },
+  {
+    id: 2,
+    tex:
+      "The snake will reverse it's direction if it will eat the food of red color",
+  },
+  {
+    id: 3,
+    tex:
+      "Your game will be over if you get stuck with the wall or bite the snake's body",
+  },
+];
